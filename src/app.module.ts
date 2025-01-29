557997bc09471d842f8cb38d2bd3afa00bd2203c
savfa <savfa@yandex.ru>
2025-01-30 00:55:17 +0300

    add global prefix `api`, fix prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { RouterModule } from '@nestjs/core';
 
 @Module({
-  imports: [
-    AuthModule,
-    UsersModule,
-    RouterModule.register([
-      {
-        path: `api`,
-        children: [AuthModule, UsersModule],
-      },
-    ]),
-  ],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [],
 })
