ba21326771bc500014dc09ce6d0de4030e69c837
savfa <savfa@yandex.ru>
2025-01-29 16:07:06 +0300

    add auth and user sources. Add swagger api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    RouterModule.register([
+      {
+        path: `api`,
+        children: [AuthModule, UsersModule],
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
